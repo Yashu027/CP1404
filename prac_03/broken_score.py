e3ad6105c2d3ed7da6ed9393e596b5351b0137c4
yashwanth korimerla <yashwanthkumar.korimerla@my.jcu.edu.au>
2022-07-31 17:19:57 +0800

    Refactor broken score program to use functions and display random score

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,10 +1,38 @@
-score = float(input("Enter score: "))
+import random
 
-if score > 100 or score < 0:
+
+def main():
+    score = float(input("Enter score: "))
+
+    display_results(score)
+
+
+def display_results(score):
+    if score > 100 or score < 0:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+def random_result():
+    return random.randint(1, 100)
+
+
+random_score = random_result()
+
+print(random_score)
+
+if random_score > 100 or random_score < 0:
     print("Invalid score")
-elif score >= 90:
+elif random_score >= 90:
     print("Excellent")
-elif score >= 50:
+elif random_score >= 50:
     print("Passable")
 else:
     print("Bad")
+
+main()
