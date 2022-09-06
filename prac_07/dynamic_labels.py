e71d38d3acacdbaf32e890ff22afc1002611b954
yashwanth korimerla <yashwanthkumar.korimerla@my.jcu.edu.au>
2022-09-06 17:58:33 +0800

    Add dynamic_labels.kv and dynamic_labels.py

---
+++ prac_07/dynamic_labels.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabel(App):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ["Bob Brown", "Cat Cyan", "Oren Ochre"]
+
+    def build(self):
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file("dynamic_labels.kv")
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widget(temp_label)
+
+
+DynamicLabel().run()
